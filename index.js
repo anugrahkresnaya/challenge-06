d14804cea2aedd104f3bc076ddd8545f45a3fcaf
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-10-06 19:17:47 +0700

    feat: add multer and cloudinary to post data

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-const { Cars1 } = require('./models')
+const { Cars } = require('./models')
+const upload = require('./server/multer')
+const cloudinary = require('./server/cloudinary')
 const port = 8002
 
 app.use(express.json())
 
 //get cars
-app.get('/cars', (req, res) => {
-  Cars1.findAll()
+app.get('/', (req, res) => {
+  Cars.findAll()
     .then(cars => {
       res.status(200).json(cars)
     })
@@ -15,7 +17,7 @@ app.get('/cars', (req, res) => {
 
 //get by id
 app.get('/cars/:id', (req, res) => {
-  Cars1.findOne({
+  Cars.findOne({
     where: {id: req.params.id}
   })
     .then(cars => {
@@ -30,28 +32,44 @@ app.get('/cars/:id', (req, res) => {
 })
 
 //post cars
-app.post('/cars', (req, res) => {
-  Cars1.create({
-    name: req.body.name,
-    price: req.body.price,
-    size: req.body.size,
-    image: req.body.image
+app.post('/cars', upload, (req, res) => {
+  const fileBase64 = req.file.buffer.toString('base64');
+  const file = `data:${req.file.mimetype};base64,${fileBase64}`;
+
+  cloudinary.uploader.upload(file, function (err, result) {
+    if (!!err) {
+      console.log(err)
+      return res.status(400).json({
+        message: "gagal"
+      })
+    }
+    Cars.create({
+      name: req.body.name,
+      price: req.body.price,
+      size: req.body.size,
+      image: result.url
+    })
+      .then(car => {
+        res.status(201).json({
+          message: "sukses",
+          data: car
+        })
+      })
+      .catch(err => {
+        res.status(422).json('cant create car')
+      })
   })
-    .then(car => {
-      res.status(201).json(car)
-    })
-    .catch(err => {
-      res.status(422).json('cant create car')
-    })
+
 })
 
 // put cars
 app.put('/cars/:id', (req, res) => {
-  Cars1.update({
+  const url = `/uploads/${req.file.filename}`
+  Cars.update({
     name: req.body.name,
     price: req.body.price,
     size: req.body.size,
-    image: req.body.image
+    image: url
   }, {
     where: { id: req.params.id }
   })
@@ -63,6 +81,40 @@ app.put('/cars/:id', (req, res) => {
     })
 })
 
+// put endpoint upload file
+app.put('/cars/:id/image/cloudinary', upload, (req, res) => {
+  const fileBase64 = req.file.buffer.toString('base64');
+  const file = `data:${req.file.mimetype};base64,${fileBase64}`;
+
+  cloudinary.uploader.upload(file, function (err, result) {
+    if (!!err) {
+      console.log(err)
+      return res.status(400).json({
+        message: "gagal"
+      })
+    }
+
+    res.status(201).json({
+      message: "sukses",
+      url: result.url,
+    })
+  })
+})
+
+app.delete('/cars/:id', (req, res) => {
+  Cars.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((car) => {
+      res.status(200).json(car)
+    })
+    .catch(err => {
+      res.status(422).json(err)
+    })
+})
+
 app.listen(port, () => {
-  console.log('server jalan')
+  console.log(`server up on http://localhost:${port}`)
 })
