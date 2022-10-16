efb52b3a3d53ec85b4234882b76e287ca0e87b1e
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2022-10-16 18:09:20 +0700

    refactor: add method for get car by id

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,39 @@
 const express = require('express');
+const controllers = require('../app/controllers');
 const apiRouter = express.Router();
 
-apiRouter.get('/api/v1/cars');
-apiRouter.post('/api/v1/cars');
-apiRouter.put('/api/v1/cars/:id');
-apiRouter.get('/api/v1/cars/:id');
-apiRouter.delete('/api/v1/cars/:id');
+apiRouter.get(
+  '/api/v1/cars',
+  controllers.api.v1.car.list
+);
+apiRouter.post(
+  '/api/v1/cars',
+  controllers.api.v1.car.create
+);
+apiRouter.put(
+  '/api/v1/cars/:id',
+  controllers.api.v1.car.setCar,
+  controllers.api.v1.car.update
+);
+apiRouter.get(
+  '/api/v1/cars/:id',
+  controllers.api.v1.car.getCar
+);
+apiRouter.delete(
+  '/api/v1/cars/:id',
+  controllers.api.v1.car.setCar,
+  controllers.api.v1.car.destroy
+);
 
+// error handler
+apiRouter.get('/api/v1/errors', () => {
+  throw new Error(
+    "Error ini"
+  )
+});
+
+apiRouter.use(controllers.api.main.onLost);
+apiRouter.use(controllers.api.main.onError);
 
 module.exports = apiRouter;
 
