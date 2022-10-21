24387162298b7fdbcc9b27d1c1a50035c8e2528c
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2022-10-21 13:38:09 +0700

    fix: add superadmin with seeders and bcrypt for password

---
+++ db/seeders/20221021062812-superAdmin.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Users', [{
+      email: 'superadmin123@gmail.com',
+      password: bcrypt.hashSync('super123', 10),
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
